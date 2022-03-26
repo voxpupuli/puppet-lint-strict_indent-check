3e7a7101d19f4a5ea796698fab396af06bb6b4af
Jan-Otto Kröpke <github@jkroepke.de>
2022-03-26 10:19:57 +0100

    add more heredoc examples

--- spec/fixtures/pass/heredoc.pp
+++ spec/fixtures/pass/heredoc.pp
@@ -19,3 +19,23 @@ case fact('os.family') {
       | EOT
   }
 }
+
+file { 'file':
+  content => @(FILE),
+    $signed_header_fields{'received'} = 0;
+    $enable_dkim_verification = 1;
+    $enable_dkim_signing = 1;
+    @client_ipaddr_policy = (
+        \@mynetworks => 'MYNETS',
+    );
+    $policy_bank{'MYNETS'} = {
+        originating => 1,
+        final_virus_destiny => D_REJECT,
+        final_spam_destiny => D_REJECT,
+        final_bad_header_destiny => D_PASS,
+        terminate_dsn_on_notify_success => 0,
+        warnbadhsender => 1,
+    };
+    | FILE
+  ;
+}
